58a26eb333912fb8292222aace9984dfd4917300
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2017-05-07 11:11:37 +0200

    Add brand WP8 icons

--- generate-icons.js
+++ generate-icons.js
@@ -3,7 +3,7 @@ const svg = require('svgexport');
 const iconOrange = 'icon-orange.svg';
 const iconWhite = 'icon-white.svg';
 
-const labelSizeMap = {
+const androidLabelSizeMap = {
     xxxhdpi: 192,
     xxhdpi: 144,
     xhdpi: 96,
@@ -15,8 +15,19 @@ const labelSizeMap = {
 const androidIcon = (label) => [
     iconOrange,
     path.join('www', 'res', 'icon', 'android', `drawable-${label}-icon.png`),
-    `${labelSizeMap[label]}:${labelSizeMap[label]}`
+    `${androidLabelSizeMap[label]}:${androidLabelSizeMap[label]}`
 ];
+
+const wp8LabelSizeMap = {
+    ApplicationIcon: 99,
+    Background: 159
+};
+
+const wp8Icon = (label) => [
+    iconOrange,
+    path.join('www', 'res', 'icon', 'wp8', `${label}.png`),
+    `${wp8LabelSizeMap[label]}:${wp8LabelSizeMap[label]}`
+]
 
 const args = [
     [
@@ -24,7 +35,8 @@ const args = [
         path.join('www', 'img', 'splash-icon.png'),
         '200:200'
     ],
-    ...Object.keys(labelSizeMap).map(androidIcon)
+    ...Object.keys(wp8LabelSizeMap).map(wp8Icon),
+    ...Object.keys(androidLabelSizeMap).map(androidIcon),
 ]
 
 args.forEach((argSet) => svg.cli(argSet));
