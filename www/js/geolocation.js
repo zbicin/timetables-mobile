313e69c752124f99bc782904d9673adaf9c20ba6
Krzysztof Zbicinski <fistasheq+github@gmail.com>
2017-06-23 23:12:29 +0200

    Restore real location fetching

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -2,13 +2,7 @@ import { Promise } from 'bluebird';
 
 const getCurrentPosition = () => new Promise((resolve, reject) => {
     const timeout = 10000;
-    //navigator.geolocation.getCurrentPosition(resolve, reject, { timeout });
-    resolve({
-        coords: {
-            latitude: 0,
-            longitude: 0
-        }
-    })
+    navigator.geolocation.getCurrentPosition(resolve, reject, { timeout });
 });
 
 export const Geolocation = {
