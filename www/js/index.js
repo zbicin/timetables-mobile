d2795e34b1c781126561864685bb08a5ceafbe56
Krzysztof Zbicinski <fistasheq+github@gmail.com>
2017-08-01 19:43:50 +0200

    Use real data

--- www/js/index.js
+++ www/js/index.js
@@ -3,8 +3,8 @@ import style from '../css/style.css'
 
 import 'core-js';
 import { DOMHelper } from './dom';
-import { DummyTimetables as Timetables } from './timetables.dummy';
-// import { Timetables } from './timetables';
+// import { DummyTimetables as Timetables } from './timetables.dummy';
+import { Timetables } from './timetables';
 import { Card } from './card';
 
 import { Promise } from 'bluebird';
