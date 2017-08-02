c25405624420949de67d2bd4c763b7af7ae6dd42
Krzysztof Zbicinski <fistasheq+github@gmail.com>
2017-08-02 21:36:34 +0200

    Extract UI module

--- www/js/index.js
+++ www/js/index.js
@@ -3,32 +3,22 @@ import style from '../css/style.css'
 
 import 'core-js';
 import 'konami-code-js';
-import { DOMHelper } from './dom';
+import { UI } from './ui';
 // import { DummyTimetables as Timetables } from './timetables.dummy';
 import { Timetables } from './timetables';
-import { Card } from './card';
 
 import { Promise } from 'bluebird';
 Promise.config({
     cancellation: true
 });
 
-const card = Object.create(Card);
-const dom = Object.create(DOMHelper);
-const debugModeKey = 'debug';
+const noop = () => {};
 const pendingPromises = new Set();
 const refreshIntervalInSeconds = 30;
+const ui = Object.create(UI);
 const timetables = Object.create(Timetables);
 
-let cardsContainer;
-let cardsHandles;
-let debugConsole;
 let lastRefreshTime;
-let menuRefresh;
-let progressBar;
-let progressBarInner;
-let retryButton;
-let splashElement;
 let refreshHandle;
 
 const cleanupHandles = () => {
@@ -37,128 +27,33 @@ const cleanupHandles = () => {
     pendingPromises.clear();
 };
 
-const log = (message) => {
-    const line = dom.create('div', message);
-    debugConsole.appendChild(line);
-    debugConsole.scrollTop = debugConsole.scrollHeight;
-    console.log(message);
-};
-
 const onError = (e) => {
-    let errorMessage;
-    let information;
-
-    if (e instanceof XMLHttpRequest) {
-        errorMessage = e.statusText || e.status;
-        information = `Wystąpił problem z połączeniem internetowym. Sprawdź ustawienia telefonu i spróbuj ponownie (${errorMessage}).`;
-    } else if (e.toString().indexOf('PositionError') > -1) {
-        errorMessage = e.code;
-        information = `Nie udało się ustalić Twojego położenia. Sprawdź ustawienia lokalizacji w swoim urządzeniu i spróbuj ponownie (kod błędu: ${errorMessage}).`;
-    } else {
-        errorMessage = e.message || e.code || e;
-        information = `Nie udało się pobrać danych przystanków w okolicy. Upewnij się, że masz włączone usługi lokalizacji oraz dostęp do Internetu, a następnie uruchom ponownie aplikację. (${errorMessage})`;
-    }
-    navigator.notification.alert(information, null, '¯\\_(ツ)_/¯');
-    console.error(e);
-    log(errorMessage);
-
+    ui.showErrorMessage(e);
     cleanupHandles();
-
-    if (progressBar) {
-        progressBar.setAttribute('hidden', true);
-    }
-    if (splashElement) {
-        retryButton.removeAttribute('hidden');
-        splashElement.classList.remove('animate');
-    }
-    if (menuRefresh) {
-        menuRefresh.classList.remove('animate');
-    }
-};
-
-const updateConsoleVisibility = () => {
-    if (localStorage.getItem(debugModeKey)) {
-        debugConsole.removeAttribute('hidden');
-    } else {
-        debugConsole.setAttribute('hidden', 'true');
-    }
 };
 
 const isPending = () => pendingPromises.size > 0;
 
-const formatTime = (date) => {
-    const twoDigits = (input) => input < 10 ? '0' + input : '' + input;
-    return [date.getHours(), date.getMinutes(), date.getSeconds()]
-        .map((segment) => segment < 10 ? '0' + segment : '' + segment)
-        .join(':');
-};
-
-const updateRefreshState = () => {
-    if (lastRefreshTime && isPending()) {
-        menuRefresh.classList.add('animate');
-    }
-    else {
-        menuRefresh.classList.remove('animate');
-    }
-};
-
-const onFetchUpdate = (progress) => {
-    progressBarInner.style.width = `${progress * 100}%`;
-};
-
 const timeout = (data, timeout) => new Promise((resolve) => setTimeout(() => resolve(data), timeout));
 
-const refreshView = (onRefresh) => {
-    log('refreshView');
-    const promise = timetables.fetchNearbyTimetables(onFetchUpdate)
+const refresh = (onRefresh = noop) => {
+    ui.log('refresh');
+    const promise = timetables.fetchNearbyTimetables(ui.onFetchUpdate)
         .then((boardsData) => timeout(boardsData, 100))
         .then((boardsData) => {
-            log('rendering boards');
-            pendingPromises.delete(promise);
-            updateRefreshState();
-
-            cardsHandles = dom.$all('.card');
-            if (cardsHandles.length === 0) {
-                cardsHandles = renderBoards(boardsData);
-            }
-            else {
-                updateBoards(boardsData, cardsHandles);
-            }
             if (!refreshHandle) {
-                refreshHandle = setupRefresh(cardsHandles);
+                refreshHandle = setupRefresh(ui.elements.cards);
             }
+            pendingPromises.delete(promise);
+            ui.renderBoards(boardsData);
             lastRefreshTime = new Date();
-            progressBar.setAttribute('hidden', true);
-
-            if (onRefresh) {
-                onRefresh();
-            }
+            onRefresh();
         }).catch((error) => {
             pendingPromises.delete(promise);
-            updateRefreshState();
             onError(error);
         });
     pendingPromises.add(promise);
-    updateRefreshState();
-};
-
-const renderBoards = (boards) => {
-    const container = cardsContainer;
-    const fragment = document.createDocumentFragment();
-    const cards = boards.map(card.buildFullCard);
-
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-
-    cards.forEach((card) => fragment.appendChild(card));
-    container.appendChild(fragment);
-
-    return cards;
-};
-
-const updateBoards = (boardsData, cardsHandles) => {
-    boardsData.forEach((boardData, index) => card.update(cardsHandles[index], boardData));
+    ui.updateRefreshState(lastRefreshTime, isPending());
 };
 
 const setupRefresh = (cardsHandles) => {
@@ -166,55 +61,30 @@ const setupRefresh = (cardsHandles) => {
 
     return setInterval(() => {
         if (!isPending()) {
-            refreshView();
+            refresh();
         }
     }, refreshInterval);
 };
 
-const waitAndHideSplash = () => {
-    const splash = dom.$('#splash');
-    if (splash) {
-        splash.addEventListener('transitionend', () => {
-            log('splash.transitionend');
-            splash.parentNode.removeChild(splash);
-        });
-        splash.classList.add('hidden');
-    }
-};
-
-const onRefresh = (e) => {
+const onRefreshButton = (e) => {
     if (!isPending()) {
-        refreshView();
+        refresh();
     }
 };
 
 const onInfo = (e) => {
-    let version;
-
-    cordova.getAppVersion()
-        .catch(() => version = 'N/A')
-        .then((v) => {
-            version = version || v;
-            let information = `Aplikacja wyświetla na żywo tablice rozkładowe przystanków znajdujących się w okolicy. Pobiera informacje z serwisu rozklady.lodz.pl i przedstawia je w wygodnej formie.\n\nDane odświeżane są automatycznie co ${refreshIntervalInSeconds} sekund.`;
-            if (lastRefreshTime) {
-                information += ` Ostatnia aktualizacja danych: ${formatTime(lastRefreshTime)}.`;
-            }
-            information += `\n\nWersja aplikacji: ${version}\n`;
-            information += 'Kontakt: tabliceprzystankowe@gmail.com\n\nAutorem ikony "Bus" udostępnionej na bazie licencji CC 3.0 BY US jest Nikita Kozin.\nhttps://creativecommons.org/licenses/by/3.0/us/';
-
-            navigator.notification.alert(information, null, 'Tablice Przystankowe');
-        })
+    ui.showInfoMessage(lastRefreshTime, refreshIntervalInSeconds);
 };
 
 const onPause = () => {
-    log('device.pause');
+    ui.log('device.pause');
     cleanupHandles();
 };
 
 const onResume = () => {
-    log('device.resume');
+    ui.log('device.resume');
     cleanupHandles();
-    refreshView(waitAndHideSplash);
+    refresh(ui.waitAndHideSplash);
 };
 
 const onKonamiCode = () => {
@@ -224,7 +94,7 @@ const onKonamiCode = () => {
         localStorage.setItem(debugModeKey, true);
     }
 
-    updateConsoleVisibility();
+    ui.updateConsoleVisibility();
 };
 
 const onRetryButton = () => {
@@ -232,28 +102,18 @@ const onRetryButton = () => {
 };
 
 const onDeviceReady = () => {
-    if (cordova.platformId == 'android') {
-        StatusBar.backgroundColorByHexString('ee8801');
-    }
-
-    cardsContainer = dom.$('.cards');
-    debugConsole = dom.$('.debug-console');
-    menuRefresh = dom.$('#menu-refresh');
-    progressBar = dom.$('.progress-bar');
-    progressBarInner = dom.$('.progress-bar-inner');
-    retryButton = dom.$('#retry-button');
-    splashElement = dom.$('#splash');
-
-    dom.$('#menu-info').addEventListener('click', onInfo);
-    menuRefresh.addEventListener('click', onRefresh);
-    retryButton.addEventListener('click', onRetryButton);
+    ui.init();
+    
+    ui.elements.menuInfo.addEventListener('click', onInfo);
+    ui.elements.menuRefresh.addEventListener('click', onRefreshButton);
+    ui.elements.retryButton.addEventListener('click', onRetryButton);
     document.addEventListener('konamiCode', onKonamiCode);
     document.addEventListener('pause', onPause);
     document.addEventListener('resume', onResume);
 
-    log('document.deviceready');
-    refreshView(waitAndHideSplash);
-    updateConsoleVisibility();
+    ui.log('document.deviceready');
+    refresh(ui.waitAndHideSplash);
+    ui.updateConsoleVisibility();
 };
 
 document.addEventListener('deviceready', onDeviceReady);
