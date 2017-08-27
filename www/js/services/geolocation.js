d70b03ce4d86163e514d6bab962035720b55c038
Krzysztof Zbicinski <fistasheq+github@gmail.com>
2017-08-27 17:12:23 +0200

    Enable high accuracy in Geolocation

--- www/js/services/geolocation.js
+++ www/js/services/geolocation.js
@@ -3,8 +3,9 @@ import { Promise } from 'bluebird';
 export class Geolocation {
     static getCurrentPosition() {
         return new Promise((resolve, reject) => {
+            const enableHighAccuracy = true;
             const timeout = 10000;
-            navigator.geolocation.getCurrentPosition(resolve, reject, { timeout });
+            navigator.geolocation.getCurrentPosition(resolve, reject, { enableHighAccuracy, timeout });
         });
     }
 };
