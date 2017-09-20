4bf8d5e840fc9746345022f4039fc6d161779c8b
Krzysztof Zbicinski <fistasheq+github@gmail.com>
2017-09-20 16:55:33 +0200

    Fix index generation

--- scripts/webpack-generate-indexes.js
+++ scripts/webpack-generate-indexes.js
@@ -1,22 +1,62 @@
+const fs = require('fs');
 const path = require('path');
 const generateIndexes = require('./generate-indexes');
+
+const isDirectory = (source) => fs.lstatSync(source).isDirectory();
+const flatten = (a, b) => a.concat(b);
+
+function getDirectoriesFromWatcher(watcher) {
+    const files = Object.keys(watcher.mtimes);
+    const result = files
+        .map((f) => path.dirname(f))
+        .filter((d, i, self) => self.indexOf(d) === i)
+        .filter((d) => ignorePaths.indexOf(d) === -1);
+    return result;
+}
+
+function getDirectoriesFromPath(p) {
+    const mainResult = fs.readdirSync(p)
+        .map(name => p.join(p, name))
+        .filter(isDirectory);
+    const subResult = mainResult
+        .map((path) => getDirectoriesFromPath(path))
+        .reduce(flatten, [])
+        .filter((a) => a.length > 0);
+        
+    const result = mainResult.concat(subResult);
+    return result;
+}
 
 function WebpackGenerateIndexes(ignorePaths = []) {
     let apply = (compiler) => {
         compiler.plugin('watch-run', (watching, cb) => {
+            console.log('watch-run');
             const watcher = watching.compiler.watchFileSystem.wfs.watcher;
+            let uniqueDirectories = [];
             if(watcher) {
-                const files = Object.keys(watcher.mtimes);
-                const uniqueDirectories = files
-                    .map((f) => path.dirname(f))
-                    .filter((d, i, self) => self.indexOf(d) === i)
-                    .filter((d) => ignorePaths.indexOf(d) === -1);
+                uniqueDirectories = getDirectoriesFromWatcher(watcher);
+            } else {
+                let rootPath = path.dirname(compiler.options.entry.app);
+                uniqueDirectories = getDirectoriesFromPath(rootPath);
+            }
+            
+            if(uniqueDirectories.length) {
+                console.log('Generating indexes for: ');
+                uniqueDirectories.forEach((d) => console.log(d));
+                generateIndexes(uniqueDirectories);
+            }
+            cb();
+        });
 
-                if(uniqueDirectories.length) {
-                    console.log('Generating indexes for: ');
-                    uniqueDirectories.forEach((d) => console.log(d));
-                    generateIndexes(uniqueDirectories);
-                }
+        compiler.plugin('run', (watching, cb) => {
+            console.log('run');
+            let rootPath = path.dirname(compiler.options.entry.app);
+            let uniqueDirectories = getDirectoriesFromPath(rootPath);
+            
+            if(uniqueDirectories.length) {
+                console.log('Generating indexes for: ');
+                uniqueDirectories.forEach((d) => console.log(d));
+                generateIndexes(uniqueDirectories);
             }
             cb();
         });
